6b0fe69d4e74a764f9798e9557b783bbbebd229c
Aravind Balla <bsaaravind@gmail.com>
2017-11-30 14:27:07 +0530

    Add Jest Framework and dummt test case

--- src/logic.js
+++ src/logic.js
@@ -1,18 +1,9 @@
 const fs = require('fs');
 const axios = require('axios');
 
-const publish = (filename) => {
-  const content = fs.readFileSync(filename, 'utf-8');
-  
-  const data = {
-    "description": "the description for this gist",
-    "public": true,
-    "files": {
-    }
-  }
-  data.files[filename] = { 'content': content};
-
-  axios.post('https://api.github.com/gists', data)
+export async function publish(filename) {
+  const postData = await getPostData(filename);
+  axios.post('https://api.github.com/gists', postData)
     .then((response) => {
       console.log('A gist was sucessfully created..');
       console.log(response.data.html_url);
@@ -22,6 +13,13 @@ const publish = (filename) => {
     })
 };
 
-module.exports = {
-  publish
+export function getPostData(filename) {
+  const content = fs.readFileSync(filename, 'utf-8');
+  let data = {
+    "description": "",
+    "public": true,
+    "files": {}
+  }
+  data.files[filename] = {'content': content};
+  return data;
 }
---
+++ src/logic.test.js
@@ -0,0 +1,7 @@
+import {getPostData} from './logic.js'
+
+test('typeof content should be string', () => {
+  let content = getPostData('test-file.js').files['test-file.js'].content;
+  expect(typeof content).toBe('string');
+});
+
